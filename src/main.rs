29c9544c2d0a9e8af37f16b96a0386d0d170ed32
maetaro <maetaro@users.noreply.github.com>
2021-02-24 08:48:34 +0900

    Eliminate clippy warnings.
    * delete add function.
    * fold function to sum function.
    * delete unnecessary return.

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@ fn main() {
     let y = shuffle(x);
     assert_eq!(y.len(), 3);
 }
-fn add(a: i32, b: i32) -> i32 {
-    a + b
-}
 fn shuffle(x: Vec<i32>) -> Vec<i32> {
     use rand::Rng;
     use std::collections::HashSet;
@@ -20,7 +17,7 @@ fn shuffle(x: Vec<i32>) -> Vec<i32> {
     for (i, v) in tmp.iter().enumerate() {
         y[i] = x[*v];
     }
-    return y;
+    y
 }
 
 
@@ -29,18 +26,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn add_with_doing() {
-        let x = 10;
-        let result = add(x, 20);
-        assert_eq!(result, 30);
-    }
-
-    #[test]
     fn shuffle1() {
         let x = vec![1, 2, 3];
         let y = shuffle(x);
         assert_eq!(y.len(), 3);
-        assert_eq!(y.iter().fold(0, |sum, a| sum + a), 6);
+        let actual: i32 = y.iter().sum();
+        assert_eq!(actual, 6);
     }
 
 }
